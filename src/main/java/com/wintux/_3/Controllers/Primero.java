a4988da8cd802958b3149d2ebdb6f22cfa2fcebf
WinTux <rusok.software@gmail.com>
2024-10-24 21:22:43 -0400

    application.properties

--- src/main/java/com/wintux/_3/Controllers/Primero.java
+++ src/main/java/com/wintux/_3/Controllers/Primero.java
@@ -1,14 +1,18 @@
 package com.wintux._3.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Primero {
+	@Value("${valor.secreto}")
+	private String val;
+	
 	@RequestMapping("/") // http://localhost:8080/ [GET]
 	@ResponseBody
 	public String saludarAtodos() {
-		return "Hola a todos!! :D";
+		return "Hola a todos!! :D "+ val;
 	}
 }
