f7134d2584b05414a337b81c668554e05f12bc80
WinTux <rusok.software@gmail.com>
2024-10-25 20:41:43 -0400

    Deteniendo nuestro servicio mediante endpoints

--- src/main/java/com/wintux/_3/Controllers/Primero.java
+++ src/main/java/com/wintux/_3/Controllers/Primero.java
@@ -1,6 +1,9 @@
 package com.wintux._3.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Primero {
 	@Value("${valor.secreto}")
 	private String val;
+	@Autowired
+	private ApplicationContext context;
 	
 	@RequestMapping("/") // http://localhost:8080/ [GET]
 	@ResponseBody
 	public String saludarAtodos() {
 		return "Hola a todos!! :D "+ val;
 	}
+	@RequestMapping("/cerrar") // http://localhost:8080/cerrar [GET]
+	@ResponseBody
+	public void cerrar() {
+		SpringApplication.exit(context);
+	}
+	
 }
