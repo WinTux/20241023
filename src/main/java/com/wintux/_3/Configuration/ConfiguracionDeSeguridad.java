4e33b2a598667a895ae8e2dba62aeb95d953a8f4
WinTux <rusok.software@gmail.com>
2024-11-04 21:03:30 -0400

    HTTPS con SSL

--- src/main/java/com/wintux/_3/Configuration/ConfiguracionDeSeguridad.java
+++ src/main/java/com/wintux/_3/Configuration/ConfiguracionDeSeguridad.java
@@ -1,10 +1,13 @@
 package com.wintux._3.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.wintux._3.Filtros.ControlConexionFilter;
 
@@ -21,5 +24,13 @@ public class ConfiguracionDeSeguridad {
 		fil.addUrlPatterns("/estudiante/*");
 		fil.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return fil;
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.securityMatcher(EndpointRequest.toAnyEndpoint())
+				.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+				.build();
 	}
 }
