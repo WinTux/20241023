318ddd0f020d94f05f49c866cb7d2c70a97a680a
WinTux <rusok.software@gmail.com>
2024-11-04 20:49:01 -0400

    Agergando anotación Bean en filtro

--- src/main/java/com/wintux/_3/Configuration/ConfiguracionDeSeguridad.java
+++ src/main/java/com/wintux/_3/Configuration/ConfiguracionDeSeguridad.java
@@ -2,6 +2,7 @@ package com.wintux._3.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 
@@ -13,7 +14,7 @@ import jakarta.servlet.Filter;
 public class ConfiguracionDeSeguridad {
 	@Autowired
 	private ControlConexionFilter filtro;
-	
+	@Bean
 	FilterRegistrationBean<Filter> miFilterRegBean(){
 		FilterRegistrationBean<Filter> fil = new FilterRegistrationBean<Filter>();
 		fil.setFilter(filtro);
